44cfb8ad4bdef72032ff8ca58d3bff1e97e310fa
Mitch Negus <mnegus@sandia.gov>
2025-01-21 10:30:02 -0700

    Enable model component installation objects to use custom consoles

--- src/firewheel/control/model_component_iterator.py
+++ src/firewheel/control/model_component_iterator.py
@@ -7,17 +7,20 @@ class ModelComponentIterator:
     This class iterates over the various repositories looking for model components.
     """
 
-    def __init__(self, repositories):
+    def __init__(self, repositories, console=None):
         """
         Initialize the path iterator.
 
         Args:
             repositories (list_iterator): The list of repositories.
+            console (rich.console.Console): A console to use for displaying
+                model component information to the user.
         """
         self.path_iter = ModelComponentPathIterator(repositories)
+        self._mc_console = console
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return ModelComponent(path=next(self.path_iter))
+        return ModelComponent(path=next(self.path_iter), console=self._mc_console)
