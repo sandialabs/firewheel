326d50b534071710747958d31ee36312e3057591
Mitch Negus <mnegus@sandia.gov>
2026-01-19 16:46:25 +0000

    WIP: Update image upload tests with refactoring changes

--- src/firewheel/tests/unit/control/test_model_component_image_upload.py
+++ src/firewheel/tests/unit/control/test_model_component_image_upload.py
@@ -4,6 +4,7 @@ import shutil
 import tempfile
 import unittest
 from datetime import datetime
+from unittest.mock import patch
 
 import yaml
 
@@ -64,79 +65,102 @@ echo 'Hello, World!'
         # remove the temp directories
         shutil.rmtree(self.tmpdir)
 
+    def _get_image_store_files(self):
+        return [image[self.fn_key] for image in self.image_store.list_contents()]
+
     def test_upload_image_no_manifest_property(self):
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
         )
-        del mcp.manifest["images"]
+        del mc.manifest["images"]
 
-        res = mcp._upload_images()
-        self.assertFalse(res)
+        mc.upload_files()
+        self.assertEqual(self._get_image_store_files(), [])
 
     def test_missing_image(self):
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
         )
 
-        mcp.manifest["images"][0] = {"paths": ["invalid"]}
+        mc.manifest["images"][0] = {"paths": ["invalid"]}
         with self.assertRaises(MissingImageError):
-            mcp._upload_images()
+            mc.upload_files()
 
     def test_upload_image(self):
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
         )
 
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [])
+        self.assertEqual(self._get_image_store_files(), [])
 
-        res = mcp._upload_images()
-        self.assertEqual(res, {self.image_file_name: "no_date"})
+        mc.upload_files()
 
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [self.image_file_name])
+        self.assertEqual(self._get_image_store_files(), [self.image_file_name])
 
-    def test_double_upload_image(self):
-        mcp = ModelComponent(
+    def test_existing_images_not_reuploaded_single(self):
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
         )
+        with patch.object(
+            mc.image_store, "add_image_file", wraps=mc.image_store.add_image_file
+        ) as mock_image_upload_method:
+            self.assertEqual(self._get_image_store_files(), [])
 
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [])
+            mc.upload_files()
+            mock_image_upload_method.assert_called_once()
+            self.assertEqual(self._get_image_store_files(), [self.image_file_name])
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "no_date"})
+            mock_image_upload_method.reset_mock()
+            mc.upload_files()
+            # Ensure no uploads occurred after the reset (image exists)
+            mock_image_upload_method.assert_not_called()
+            self.assertEqual(self._get_image_store_files(), [self.image_file_name])
 
-        first_upload_time = self.image_store.get_file_upload_date(self.image_file_name)
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [self.image_file_name])
+    def test_existing_images_not_reuploaded_double(self):
+        mc = ModelComponent(
+            path=self.mc_dir,
+            repository_db=self.repository_db,
+            image_store=self.image_store,
+        )
+        with patch.object(
+            mc.image_store, "add_image_file", wraps=mc.image_store.add_image_file
+        ) as mock_image_upload_method:
+            self.assertEqual(self._get_image_store_files(), [])
 
-        # Make sure our upload time would change if we re-uploaded
-        time.sleep(2)
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: False})
+            mc.upload_files()
+            mock_image_upload_method.assert_called_once()
+            self.assertEqual(self._get_image_store_files(), [self.image_file_name])
 
-        second_upload_time = self.image_store.get_file_upload_date(self.image_file_name)
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [self.image_file_name])
+            second_image_name = "second_image"
+            second_image_contents = "SECOND IMAGE!"
+            second_image_path = os.path.join(self.mc_dir, second_image_name)
+            time.sleep(1)
+            with open(second_image_path, "w", encoding="utf8") as fname:
+                fname.write(second_image_contents)
 
-        self.assertEqual(first_upload_time, second_upload_time)
+            old_image_list = mc.manifest["images"]
+            mc.manifest["images"] = [{"paths": [second_image_name]}]
+            mc.manifest["images"].extend(old_image_list)
+
+            mock_image_upload_method.reset_mock()
+            mc.upload_files()
+            # Ensure that only one upload occurs after reset (for the second image)
+            mock_image_upload_method.assert_called_once()
+            self.assertEqual(
+                sorted(self._get_image_store_files()),
+                sorted([self.image_file_name, second_image_name]),
+            )
 
     def test_upload_subdir_image(self):
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
@@ -153,95 +177,86 @@ echo 'Hello, World!'
         with open(image_path, "w", encoding="utf8") as fname:
             fname.write(image_contents)
 
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [])
+        self.assertEqual(self._get_image_store_files(), [])
 
-        mcp.manifest["images"][0] = {"paths": [image_rel_path]}
+        mc.manifest["images"][0] = {"paths": [image_rel_path]}
+        mc.upload_files()
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {image_rel_path: "no_date"})
-
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [image_name])
-
-        result = mcp._upload_images()
-        self.assertEqual(result, {image_rel_path: False})
+        self.assertEqual(self._get_image_store_files(), [image_name])
 
     def test_upload_new_time_same_hash(self):
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
         )
 
-        pre_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        pre_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
         pre_hash = hash_file(self.image1_path)
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "no_date"})
+        mc.upload_files()
 
-        upload_time = self.image_store.get_file_upload_date(self.image_file_name)
+        upload_timestamp = self.image_store.get_file_upload_date(self.image_file_name)
         upload_hash = self.image_store.get_file_hash(self.image_file_name)
-        self.assertEqual(upload_time, pre_time)
+        self.assertEqual(upload_timestamp, pre_timestamp)
         self.assertEqual(upload_hash, pre_hash)
 
-        # Sleep so we are sure we get a new time.
-        time.sleep(2)
+        # Rewrite the file after a delay to ensure a new timestamp
+        time.sleep(1)
         with open(self.image1_path, "w", encoding="utf8") as fname:
             fname.write(self.image1)
 
-        post_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        post_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
         post_hash = hash_file(self.image1_path)
-
-        self.assertNotEqual(pre_time, post_time)
-        self.assertTrue(upload_time < post_time)
+        self.assertTrue(pre_timestamp < post_timestamp)
+        self.assertTrue(upload_timestamp < post_timestamp)
         self.assertEqual(pre_hash, post_hash)
 
-        self.assertTrue(post_time > pre_time)
+        mc.upload_files()
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "same_hash"})
+        new_upload_timestamp = self.image_store.get_file_upload_date(self.image_file_name)
+        new_upload_hash = self.image_store.get_file_hash(self.image_file_name)
+        self.assertEqual(new_upload_timestamp, upload_timestamp)
+        self.assertEqual(new_upload_hash, upload_hash)
 
     def test_upload_new_time_new_hash(self):
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
         )
 
-        pre_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        pre_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
         pre_hash = hash_file(self.image1_path)
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "no_date"})
+        mc.upload_files()
 
-        upload_time = self.image_store.get_file_upload_date(self.image_file_name)
+        upload_timestamp = self.image_store.get_file_upload_date(self.image_file_name)
         upload_hash = self.image_store.get_file_hash(self.image_file_name)
-        self.assertEqual(upload_time, pre_time)
+        self.assertEqual(upload_timestamp, pre_timestamp)
         self.assertEqual(upload_hash, pre_hash)
 
-        # Sleep so we are sure we get a new time.
-        time.sleep(2)
+        # Rewrite the file after a delay to ensure a new timestamp and new contents
+        time.sleep(1)
         with open(self.image1_path, "w", encoding="utf8") as fname:
             fname.write("different contents")
 
-        post_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        post_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
         post_hash = hash_file(self.image1_path)
-
-        self.assertNotEqual(pre_time, post_time)
-        self.assertTrue(upload_time < post_time)
+        self.assertTrue(pre_timestamp < post_timestamp)
+        self.assertTrue(upload_timestamp < post_timestamp)
         self.assertNotEqual(pre_hash, post_hash)
 
-        self.assertTrue(post_time > pre_time)
+        mc.upload_files()
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "new_hash"})
+        new_upload_timestamp = self.image_store.get_file_upload_date(self.image_file_name)
+        new_upload_hash = self.image_store.get_file_hash(self.image_file_name)
+        self.assertNotEqual(new_upload_timestamp, upload_timestamp)
+        self.assertNotEqual(new_upload_hash, upload_hash)
 
     def test_upload_old_time_new_hash(self):
         # What is this test case accomplishing?
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
@@ -250,7 +265,7 @@ echo 'Hello, World!'
         subdir = "subdir"
         second_image_path = os.path.join(self.mc_dir, subdir, self.image_file_name)
         os.makedirs(os.path.join(self.mc_dir, subdir))
-        time.sleep(2)
+        time.sleep(1)
         with open(second_image_path, "w", encoding="utf8") as fname:
             fname.write("different contents")
 
@@ -258,10 +273,10 @@ echo 'Hello, World!'
         second_hash = hash_file(second_image_path)
 
         # Sleep so we are sure we get a new time.
-        time.sleep(2)
+        time.sleep(1)
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "no_date"})
+        mc.upload_files()
+        # self.assertEqual(result, {self.image_file_name: "no_date"})
 
         upload_time = self.image_store.get_file_upload_date(self.image_file_name)
         self.assertEqual(upload_time, second_time)
@@ -272,29 +287,29 @@ echo 'Hello, World!'
         self.assertEqual(store_hash, post_hash)
         self.assertNotEqual(second_hash, post_hash)
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: False})
+        mc.upload_files()
+        # self.assertEqual(result, {self.image_file_name: False})
+        assert False
 
     def test_upload_revert_same_file(self):
-        mcp = ModelComponent(
+        mc = ModelComponent(
             path=self.mc_dir,
             repository_db=self.repository_db,
             image_store=self.image_store,
         )
 
-        pre_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        pre_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
         pre_hash = hash_file(self.image1_path)
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "no_date"})
+        mc.upload_files()
 
-        upload_time = self.image_store.get_file_upload_date(self.image_file_name)
+        upload_timestamp = self.image_store.get_file_upload_date(self.image_file_name)
         upload_hash = self.image_store.get_file_hash(self.image_file_name)
-        self.assertEqual(upload_time, pre_time)
+        self.assertEqual(upload_timestamp, pre_timestamp)
         self.assertEqual(upload_hash, pre_hash)
 
         # Sleep so we are sure we get a new time.
-        time.sleep(2)
+        time.sleep(1)
 
         # Move the file elsewhere
         tmp_path = tempfile.mkstemp()
@@ -304,98 +319,38 @@ echo 'Hello, World!'
         with open(self.image1_path, "w", encoding="utf8") as fname:
             fname.write("different contents")
 
-        post_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        post_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
         post_hash = hash_file(self.image1_path)
-
-        self.assertNotEqual(pre_time, post_time)
-        self.assertTrue(upload_time < post_time)
+        self.assertTrue(pre_timestamp < post_timestamp)
+        self.assertTrue(upload_timestamp < post_timestamp)
         self.assertNotEqual(pre_hash, post_hash)
 
-        self.assertTrue(post_time > pre_time)
+        mc.upload_files()
 
-        result = mcp._upload_images()
-        self.assertEqual(result, {self.image_file_name: "new_hash"})
-
-        upload_time = self.image_store.get_file_upload_date(self.image_file_name)
-        upload_hash = self.image_store.get_file_hash(self.image_file_name)
+        new_upload_timestamp = self.image_store.get_file_upload_date(self.image_file_name)
+        new_upload_hash = self.image_store.get_file_hash(self.image_file_name)
+        self.assertEqual(new_upload_timestamp, post_timestamp)
+        self.assertEqual(new_upload_hash, post_hash)
 
         # Sleep so we are sure we get a new time.
-        time.sleep(2)
+        time.sleep(1)
 
         # Revert to previous file
-        rev_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
-        rev_hash = hash_file(self.image1_path)
+        pre_rev_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        pre_rev_hash = hash_file(self.image1_path)
         shutil.move(tmp_path[1], self.image1_path)
+        post_rev_timestamp = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
+        post_rev_hash = hash_file(self.image1_path)
+        # The file was moved so the original and post-reversion times should be the same
+        self.assertEqual(pre_timestamp, post_rev_timestamp)
+        # ...but the file is different than the file being reverted
+        self.assertNotEqual(pre_rev_timestamp, post_rev_timestamp)
+        self.assertNotEqual(pre_rev_hash, post_rev_hash)
+        # ...and the reverted timestamp will be older than the one in the file store
+        self.assertTrue(post_rev_timestamp < new_upload_timestamp)
 
-        post_time = datetime.utcfromtimestamp(os.path.getmtime(self.image1_path))
-        post_hash = hash_file(self.image1_path)
-
-        # The file was moved so the pre/post time should be the same
-        self.assertEqual(pre_time, post_time)
-
-        # But the file is different than the file being reverted
-        self.assertNotEqual(rev_time, post_time)
-        self.assertNotEqual(rev_hash, post_hash)
-
-        # Because the file was revered the time will be older than the upload_time
-        self.assertTrue(upload_time > post_time)
-
-        # And the file will be older than the reverted file
-        self.assertTrue(rev_time > post_time)
-
-        result = mcp._upload_images()
+        mc.upload_files()
 
         # This should be uploaded because even though the time is "older" the contents
         # are newer.
-        self.assertEqual(result, {self.image_file_name: "new_hash"})
-
-    def test_upload_image_one_not_another_from_manifest(self):
-        mcp = ModelComponent(
-            path=self.mc_dir,
-            repository_db=self.repository_db,
-            image_store=self.image_store,
-        )
-
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [])
-
-        result = mcp._upload_images()
-        self.assertTrue(result)
-
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        self.assertEqual(image_list, [self.image_file_name])
-        first_upload_date = self.image_store.get_file_upload_date(self.image_file_name)
-
-        second_image_name = "second_image"
-        second_image_contents = "SECOND IMAGE!"
-        second_image_path = os.path.join(self.mc_dir, second_image_name)
-        time.sleep(1)
-        with open(second_image_path, "w", encoding="utf8") as fname:
-            fname.write(second_image_contents)
-
-        old_image_list = mcp.manifest["images"]
-        mcp.manifest["images"] = [{"paths": [second_image_name]}]
-        mcp.manifest["images"].extend(old_image_list)
-
-        # Make sure we get different upload dates.
-        time.sleep(2)
-
-        result = mcp._upload_images()
-        self.assertTrue(result)
-
-        image_list = self.image_store.list_contents()
-        image_list = [image[self.fn_key] for image in image_list]
-        image_list.sort()
-        expected_list = [second_image_name, self.image_file_name]
-        expected_list.sort()
-
-        self.assertEqual(image_list, expected_list)
-
-        second_upload_date = self.image_store.get_file_upload_date(self.image_file_name)
-        second_image_upload_date = self.image_store.get_file_upload_date(
-            second_image_name
-        )
-        self.assertEqual(first_upload_date, second_upload_date)
-        self.assertNotEqual(second_upload_date, second_image_upload_date)
+        assert False
