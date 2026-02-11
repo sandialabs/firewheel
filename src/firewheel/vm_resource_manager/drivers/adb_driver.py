2a924a6a5292b262b9adc7f24aaa5d1ccb2eceed
gjacobu <gjacobu@sandia.gov>
2026-02-11 19:22:37 +0000

    some more updates to adb driver

--- src/firewheel/vm_resource_manager/drivers/adb_driver.py
+++ src/firewheel/vm_resource_manager/drivers/adb_driver.py
@@ -1,6 +1,17 @@
 
 from firewheel.vm_resource_manager.abstract_driver import AbstractDriver
+import json
 import adbutils
+import time
+import signal
+
+EXIT_MAGIC = "ExitCode="
+
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(signum, frame):
+    raise TimeoutException()
 
 class ADBDriver(AbstractDriver):
     def __init__(self, config, log):
@@ -82,6 +93,20 @@ class ADBDriver(AbstractDriver):
     def set_user_password(self, username, password):
         raise NotImplementedError
 
+    def _get_pid_from_stream(self, stream):
+        pid_str = ""
+        while True:
+            char = output_stream.read(1)
+            if char == "\n":
+                break
+            pid_str += char
+        
+        try:
+            pid = int(pid_str)
+            return pid
+        except ValueError:
+            return None
+        
     def execute(self, path, arg=None, env=None, input_data=None, capture_output=True):
         full_cmd = ""
 
@@ -96,11 +121,52 @@ class ADBDriver(AbstractDriver):
         full_cmd += path
         full_cmd += " "
         full_cmd += arg
-        full_cmd += " &" # Run the command asynchronously
-        self.adb_device.shell(full_cmd)
+        full_cmd += f" & echo $!; wait -n $pid; echo \"{EXIT_MAGIC}$?\"" # make it run in the background and echo the PID so we can save it
+        output_stream = self.adb_device.shell(full_cmd, stream=True)
+
+
+
+        # Get and return the PID
+        pid = self._get_pid_from_stream(output_stream)
+
+        self.output_cache[pid].setdefault(pid, {})
+        self.output_cache[pid]["stream"] = output_stream
+
+        return pid
 
     def exec_status(self, pid):
-        raise NotImplementedError
+        stream = self.output_cache[pid]["stream"]
+
+        read_timeout = 1
+        timed_out = False
+        signal.alarm(read_timeout)
+        try:
+            stdout = stream.read_until_close()
+        except TimeoutException as exc:
+            timed_out = True
+            break
+        finally:
+            # end the timeout alarm
+            signal.alarm(0)
+
+
+
+        # check if process has finished
+        exited = not timed_out
+        self.output_cache[pid]["exited"] = exited
+
+        # look for the "ExitCode=..." string in the output for exit code reporting
+        if exited:
+            idx = stdout.rfind(EXIT_MAGIC)
+            exit_code_str = stdout[idx+len(EXIT_MAGIC):]
+            exit_code = int(exit_code_str)
+            
+            self.output_cache[pid]["stdout"] += stdout[:idx]
+        else:
+            self.output_cache[pid]["stdout"] += stdout
+
+        return self.output_cache[pid]
+
 
     def store_captured_output(self, pid, output):
         raise NotImplementedError
