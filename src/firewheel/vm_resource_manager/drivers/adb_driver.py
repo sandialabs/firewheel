47873c461b6b89f2ea71ab74a3601b71936b93f7
gjacobu <gjacobu@sandia.gov>
2026-02-12 05:04:06 +0000

    adb driver is getting closer! still a lot of quality / cleanup i expect

--- src/firewheel/vm_resource_manager/drivers/adb_driver.py
+++ src/firewheel/vm_resource_manager/drivers/adb_driver.py
@@ -15,14 +15,30 @@ def timeout_handler(signum, frame):
 
 class ADBDriver(AbstractDriver):
     def __init__(self, config, log):
-        self.console_port = config["qemu_append"]["port"]
+        log.info("Config = %s", config)
+        self.console_port = config["adb_port"]
         self.adb_name = f"emulator-{self.console_port}"
         self.adb_client = adbutils.AdbClient()
         self.adb_device = self.adb_client.device(self.adb_name)
+        self._is_rooted = False
+        super().__init__(config, log)
+
 
     def connect(self):
-        self.adb_device.root()
-        return
+        sleep_time = 1
+        while not self.ping():
+            self.log.debug("Waiting for device to come online")
+            time.sleep(sleep_time)
+        self.log.debug("Getting root access on device")
+        if not self._is_rooted:
+            with self.lock:
+                self.adb_device.root()
+            self.is_rooted = True
+
+            while not self.ping():
+                self.log.debug("Waiting for device to come online")
+                time.sleep(sleep_time)
+        return 1
 
     def close(self):
         # TODO this function is not needed I think
@@ -40,15 +56,16 @@ class ADBDriver(AbstractDriver):
         """
         try:
             # "adb get-state" returns "device" when the emulator/device is online
-            result = adb_device.get_state()
-            return result.stdout.strip() == "device"
-        except Exception:
+            with self.lock:
+                result = self.adb_device.get_state()
+            return result.strip() == "device"
+        except Exception as exc:
             # Any exception (including timeout) indicates the device is not reachable
             return False
 
     def sync(self):
         # TODO this function is not needed I think
-        return
+        return 1
 
     @staticmethod
     def get_engine():
@@ -69,7 +86,8 @@ class ADBDriver(AbstractDriver):
         """
         # Use the shell command to get epoch seconds on the device.
         # Android's date supports +%s which returns seconds since epoch.
-        output = self.adb_device.shell('date +%s')
+        with self.lock:
+            output = self.adb_device.shell('date +%s')
         # The output may contain newline characters; strip them.
         secs_str = output.strip()
         secs = int(secs_str)
@@ -79,16 +97,19 @@ class ADBDriver(AbstractDriver):
 
     def set_time(self):
         cur_time_nano = int(time.time() * 1e9)
-        self.adb_device.shell(f"date -s {cur_time_nano}")
+        with self.lock:
+            self.adb_device.shell(f"date -s {cur_time_nano}")
     
     def reboot(self):
-        self.adb_device.reboot()
+        with self.lock:
+            self.adb_device.reboot()
     
     def file_flush(self):
         raise NotImplementedError
 
     def network_get_interfaces(self):
-        return json.loads(self.adb_device.shell("ip -j address"))
+        with self.lock:
+            return json.loads(self.adb_device.shell("ip -j address"))
 
     def set_user_password(self, username, password):
         raise NotImplementedError
@@ -96,7 +117,7 @@ class ADBDriver(AbstractDriver):
     def _get_pid_from_stream(self, stream):
         pid_str = ""
         while True:
-            char = output_stream.read(1)
+            char = stream.read(1).decode("utf-8")
             if char == "\n":
                 break
             pid_str += char
@@ -108,6 +129,9 @@ class ADBDriver(AbstractDriver):
             return None
         
     def execute(self, path, arg=None, env=None, input_data=None, capture_output=True):
+        # TODO we probably need a way to just use bash, but until it's installed, try and replace it with sh
+        if path == "/bin/bash":
+            path = "/bin/sh"
         full_cmd = ""
 
         if input_data is not None:
@@ -118,19 +142,21 @@ class ADBDriver(AbstractDriver):
                 full_cmd += env_var
                 full_cmd += " "
         
+        if isinstance(arg, (list, tuple)):
+            arg = adbutils._utils.list2cmdline(arg)
+
         full_cmd += path
         full_cmd += " "
         full_cmd += arg
-        full_cmd += f" & echo $!; wait -n $pid; echo \"{EXIT_MAGIC}$?\"" # make it run in the background and echo the PID so we can save it
-        output_stream = self.adb_device.shell(full_cmd, stream=True)
-
-
+        full_cmd += f" & pid=$!; echo $pid; wait $pid; echo \"{EXIT_MAGIC}$?\"" # make it run in the background and echo the PID so we can save it
+        with self.lock:
+            output_stream = self.adb_device.shell(full_cmd, stream=True)
 
         # Get and return the PID
         pid = self._get_pid_from_stream(output_stream)
 
-        self.output_cache[pid].setdefault(pid, {})
-        self.output_cache[pid]["stream"] = output_stream
+        self.output_cache[pid] = {"stream": output_stream}
+        self.output_cache[pid]['cmd'] = full_cmd # TODO debugging
 
         return pid
 
@@ -144,20 +170,23 @@ class ADBDriver(AbstractDriver):
             stdout = stream.read_until_close()
         except TimeoutException as exc:
             timed_out = True
-            break
         finally:
             # end the timeout alarm
             signal.alarm(0)
 
 
+        self.log.debug("process stdout: ****%s****", stdout)
+        self.log.debug(self.output_cache[pid])
 
         # check if process has finished
         exited = not timed_out
         self.output_cache[pid]["exited"] = exited
 
         # look for the "ExitCode=..." string in the output for exit code reporting
+        self.output_cache[pid].setdefault("stdout", "")
         if exited:
             idx = stdout.rfind(EXIT_MAGIC)
+            self.log.debug("idx=%s", idx)
             exit_code_str = stdout[idx+len(EXIT_MAGIC):]
             exit_code = int(exit_code_str)
             
@@ -179,10 +208,12 @@ class ADBDriver(AbstractDriver):
         else:
             raise ValueError("Unsupported file mode")
 
-        self.adb_device.shell(f"printf '{data}' {redirect} {filename}")
+        with self.lock:
+            self.adb_device.shell(f"printf '{data}' {redirect} {filename}")
 
     def read_file(self, filename, local_destination, mode="rb"):
-        self.adb_device.sync.pull_file(filename, local_destination)
+        with self.lock:
+            self.adb_device.sync.pull_file(filename, local_destination)
 
     def write_from_file(self, filename, local_filename, mode="w"):
         with open(local_filename) as fhand:
@@ -190,10 +221,23 @@ class ADBDriver(AbstractDriver):
         self._write(filename, data, mode)
 
     def get_os(self):
+        if self.target_os:
+            return self.target_os
+
         cmd = 'echo "'
         cmd += '$(getprop ro.product.brand) '
         cmd += 'Android '
         cmd += '$(getprop ro.build.version.release) '
         cmd += '($(getprop ro.product.model))'
         cmd += '"'
-        return self.adb_device.shell(cmd)
+
+        num_attempts = 10
+        for attempt in range(num_attempts):
+            with self.lock:
+                try:
+                    self.target_os = self.adb_device.shell(cmd)
+                    break
+                except Exception as exc:
+                    self.log.exception(exc)
+                    time.sleep(1)
+        return self.target_os
